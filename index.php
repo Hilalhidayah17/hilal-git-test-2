8fb5deab14b05b5bb0b7192215b74ac175479dbd
Hilalhidayah17 <hilalhidayah589456@gmmail.com>
2024-01-09 16:28:26 +0700

    menambahkan pragarap

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h1>membuat index</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit repudiandae minima maxime magnam culpa nulla numquam sequi obcaecati. Explicabo eaque odit perspiciatis, quaerat rerum dolor aliquid quam debitis assumenda veniam.</p>
 </body>
 </html>
